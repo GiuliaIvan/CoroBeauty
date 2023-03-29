7ef229ccfdb150e3f87a52c8a1020a94df345a7a
Giulia Ivan <eaagril@students.eaaa.dk>
2023-03-29 17:20:36 +0200

    content JS website

--- JavaScript/website.js
+++ JavaScript/website.js
@@ -1,28 +1,28 @@
 /*------------ SHOW MENU-------------*/
-const showMneu = (toggleId, navId) => {
+const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
-  /*valida se se existe as variaveis*/
+  /*validates if the variables exist*/
   if (toggle && nav) {
-    /*aqui adiciono a class show-menu na div com a class nav__menu*/
+    /*here I add the show-menu class in the div with the nav__menu class*/
     toggle.addEventListener("click", () => {
-      /*adicina a class show-menu na div que tem o id nav-menu */
+      /*add the show-menu class to the div that has the nav-menu id */
       nav.classList.toggle("show-menu");
     });
   }
 };
-showMneu("nav-toggle", "nav");
+showMenu("nav-toggle", "nav");
 
-/*------------ REMOVER MENU MOBILE -------------*/
+/*------------ REMOVE MENU MOBILE -------------*/
 const navLink = document.querySelectorAll(".nav__link");
 function linkAction() {
   const navMenu = document.getElementById("nav");
-  /*Quando um link ou o botão close for clicado a class show-menu será removido*/
+  /*When a link or close button is clicked the show-menu class will be removed*/
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*==================== DEIXA O LINK CLICADO COM A CLASS ACTIVE-LINK ====================*/
+/*==================== LEAVE THE LINK CLICKED WITH THE CLASS ACTIVE-LINK ====================*/
 const linkColor = document.querySelectorAll(".nav__link");
 
 function colorLink() {
@@ -33,7 +33,7 @@ function colorLink() {
 }
 
 linkColor.forEach((L) => L.addEventListener("click", colorLink));
-/*==================== MUDAR A COR DO HEADER ====================*/
+/*==================== CHANGE HEADER COLOR ====================*/
 function scrollHeader() {
   const scrollHeader = document.getElementById("header");
   if (this.scrollY >= 100) scrollHeader.classList.add("scroll-header");
